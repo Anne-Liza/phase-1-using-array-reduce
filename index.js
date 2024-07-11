6a1734bd3eb2f2884449cbde21e358555ba693f6
Anne <annewangui210@gmail.com>
2024-07-11 21:56:55 +0300

    phase-1-using-array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let sum = 0;
+ for(let i=0; i < batteryBatches.length; i++){
+    sum += batteryBatches[i];
+}
+const totalBatteries = sum;
+console.log(totalBatteries);
+
+
